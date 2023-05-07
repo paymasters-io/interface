76077937424afedc37b63ef8a985c088f86150c5
Peter Anyaogu <anyaogupeter601@gmail.com>
2023-05-07 15:17:18 +0100

    Update Polybase.tsx

--- pages/providers/Polybase.tsx
+++ pages/providers/Polybase.tsx
@@ -4,7 +4,7 @@ import { FC, PropsWithChildren } from "react";
 
 const PolybaseReactProvider: FC<PropsWithChildren<any>> = ({ children }) => {
   const polybase = new Polybase({
-    defaultNamespace: "paymasters-io",
+    defaultNamespace: "pk/0xfba5b3ed147d9295a081d189b2ade667b547effa0610b31dc55469b17a8c77617658497aedfdf30cd8139d4604df06978622e887bd2d86bc30469feef1664e72/pmt-waitlist",
   });
 
   return <PolybaseProvider polybase={polybase}>{children}</PolybaseProvider>;
